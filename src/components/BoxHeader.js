d31986968fea03f9bb2f2bcebdd892e61531ce5c
Kaspermn33 <32263696+Kaspermn33@users.noreply.github.com>
2022-04-06 16:25:47 +0200

    project page no functionality

---
+++ src/components/BoxHeader.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BoxHeader = (title) => {
+  return (
+    <h2 className='small-box-header'>{title.title}</h2>
+  )
+}
+
+export default BoxHeader
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,13 +1,69 @@
-import {FaFolder} from 'react-icons/fa'
+import { FaFolder } from 'react-icons/fa'
 import { useParams } from 'react-router-dom';
+import BoxHeader from './BoxHeader';
+
+
 import Header from './Header'
-const Project = ({projects}) => {
+const Project = ({ projects }) => {
   const { id } = useParams()
   const project = projects.find(project => project.id == id)
   return (
-    <div className='project'> 
-      <Header title={project.name}/>
-        <h3><FaFolder/>{project.name}</h3>
+    <div>
+      <Header title={project.name} />
+      <div className='project'>
+        <div>
+          <h1>Project</h1>
+        </div>
+        <div className='project-main'>
+          <div className='project-left'>
+            <div className='screening-box'>
+              <h2>Screening</h2>
+              <div className='box-type'>
+                <div className='new-box'>
+                <BoxHeader title={"New"} />
+                </div>
+                <div className='recent'>
+                <BoxHeader title={"Recent"} />
+                </div>
+              </div>
+
+            </div>
+            <div className='LCA-box'>
+              <h2>LCA</h2>
+              <div className='box-type'>
+                <div className='new-box'>
+                <BoxHeader title={"New"} />
+                </div>
+                <div className='recent'>
+                <BoxHeader title={"Recent"} />
+                </div>
+              </div>
+            </div>
+
+
+          </div>
+          <div className='project-right'>
+
+            <div className='costs-box'>
+              <h2>Costs</h2>
+              <div className='box-type'>
+                <div className='new-box'>
+                <BoxHeader title={"New"} />
+                </div>
+                <div className='recent'>
+                <BoxHeader title={"Recent"} />
+                </div>
+              </div>
+            </div>
+            <div className='manageproject-box'>
+              <h2>Manage project</h2>
+              <div className='manage-project-box'>
+              <BoxHeader title={"Make changes"} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
