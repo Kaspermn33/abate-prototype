936fcd6f0bc6c7a0ced104af73b5dfb5404085c0
Kaspermn33 <32263696+Kaspermn33@users.noreply.github.com>
2022-04-13 11:46:55 +0200

    added logo and logo text

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import logo from './logo.png';
 
 const Logo = () => {
   return (
-    <div className='logo-container'>Logo</div>
+    <div className='logo-container'> 
+      <img className='logo-image' src={logo} />
+      <div>
+        <h3 className='logo-text'>Spot the ABATE Solutions Aps</h3>
+      </div>
+      
+    </div>
   )
 }
 
