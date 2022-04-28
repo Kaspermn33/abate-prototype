82f12afbe6ae62f3868c3ae825a01caf61716fb7
Kaspermn33 <32263696+Kaspermn33@users.noreply.github.com>
2022-04-28 13:24:20 +0200

    started work on user component

--- src/components/User.js
+++ src/components/User.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import { BiUserCircle } from 'react-icons/bi'
+import { VscBellDot } from 'react-icons/vsc'
 
 const User = () => {
   return (
-    <div className='user'>User</div>
+    <div className='user-container'>
+      <div className='user-icon'>
+        <BiUserCircle/>
+      </div>
+      <div className='user-name'>
+        Spotty Solutionist
+      </div>
+      <div className='user-bell'>
+        <VscBellDot/>
+      </div>
+    </div>
   )
 }
 
